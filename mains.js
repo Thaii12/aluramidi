afe02aa781ddd168912c659102886d74f7bedd53
Thaii12 <128871168+Thaii12@users.noreply.github.com>
2023-10-06 21:14:10 -0300

    Update mains.js

--- mains.js
+++ mains.js
@@ -1,5 +1,15 @@
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
 function tocaSomPom(){
   document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap(){
+document.querySelector('#som_tecla_clap').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador=0;
+while(contador <listaDeTeclas.length) {
+  listaDeTeclas[contador].onclick = tocaSomPom;
+  contador = contador + 1;
+  console.log(contador);
 }
